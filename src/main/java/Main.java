bc4a4c263b364e414bb55e52a348449cc6d8189b
Rob Schwartz <robschwartz@Robs-MacBook-Pro.local>
2020-08-01 17:35:33 -0500

    Add JFrame and JTable
    Add console output for score distribution debugging

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,17 +6,21 @@ import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 
+import javax.swing.*;
+import java.awt.print.PrinterException;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.Vector;
 import java.util.concurrent.TimeUnit;
 
 
 
 public class Main {
-    private final static String[] GAMERTAGS = {"McDonalds79", "Cashius Playz", "dr treyy", "schoolboywu69", "Joevani JoJo", "kelloggking"};
-    private final static String[] PLATFORM = {"xbl", "xbl", "xbl", "xbl", "xbl", "xbl"};
+    private final static String[] GAMERTAGS = {"McDonalds79", "Cashius Playz", "dr treyy", "schoolboywu69", "Joevani JoJo", "kelloggking","Ccatania21"};
+    private final static String[] PLATFORM = {"xbl", "xbl", "xbl", "xbl", "xbl", "xbl", "xbl"};
     private final static int NUMOFGAMERTAGS = GAMERTAGS.length;
     private final static String[] FIELDS = {"kdRatio", "scorePerMinute", "topTwentyFive", "topFive", "timePlayed", "wins", "gamesPlayed"};
     private final static int NUMOFFIELDS = FIELDS.length;
@@ -45,8 +49,8 @@ public class Main {
                 }
 
                 // add players as the raw data is selected
-                playerList.add(new Player(playerData[j][0], playerData[j][1], (int)(playerData[j][1]), (int)playerData[j][1],
-                        playerData[j][1], (int)playerData[j][1], (int)playerData[j][1],GAMERTAGS[j], PLATFORM[j]));
+                playerList.add(new Player(playerData[j][0], playerData[j][1], (int)(playerData[j][2]), (int)playerData[j][3],
+                        playerData[j][4], (int)playerData[j][5], (int)playerData[j][6],GAMERTAGS[j], PLATFORM[j]));
             }
 
         }
@@ -60,10 +64,14 @@ public class Main {
         // get the calculated scores
         double[] scores = calculateScores();
 
-        System.out.println("/nPrinting out scores....");
+        // Print data to command line
+        System.out.println("\n\nPrinting out scores....");
         for (int p = 0; p < NUMOFGAMERTAGS; p++){
-            System.out.println(GAMERTAGS[p] + "    " + scores[p]);
+            System.out.println(GAMERTAGS[p] + "            " + scores[p]);
         }
+
+        // Print the data
+        printScores(scores);
 
     }
 
@@ -103,35 +111,6 @@ public class Main {
         return response;
     }
 
-    // TODO: Determine if normalizing data is something needed or not
-    // return the normalized stats
-    // players are each row and stats are each column
-    private static double[][] normalizeData(double rawStats[][]){
-        double[][] normalizedStats = new double[rawStats.length][rawStats[0].length];
-
-        double[][] rawStatsDouble = new double[rawStats.length][rawStats[0].length];
-
-
-        // for each stat
-        for(int i = 0; i< rawStatsDouble[0].length; i++){
-            // set a max val
-            double maxVal = rawStatsDouble[0][i];
-            // loop through each of the rows for each player to find max value
-            for (int j = 1; j < rawStatsDouble.length; j++){
-                if (rawStatsDouble[j][i] > maxVal){
-                    maxVal = rawStatsDouble[j][i];
-                }
-            }
-
-            // normalize the data
-            for (int j = 0; j< rawStats.length; j++){
-                normalizedStats[j][i] = rawStatsDouble[j][i]/maxVal;
-            }
-        }
-
-        return normalizedStats;
-    }
-
     // calculate the new "score" for each player using normalized stats higher score -> better player
     // "kdRatio", "scorePerMinute", "topTwentyFive", "topFive", "timePlayed", "wins", "gamesPlayed"
     private static double[] calculateScores(){
@@ -143,39 +122,67 @@ public class Main {
             double timePlayed = playerList.get(player).getTimePlayed();
             double gamesPlayed = playerList.get(player).getGamesPlayed();
 
-            // kd ratio
-            double kd = playerList.get(player).getKd() * 2500;
 
+            System.out.println("\nDATA START FOR " + playerList.get(player).getGamertag());
+            // kd ratio
+            double kd = playerList.get(player).getKd() * 250;
+            System.out.println("KD*250: " + kd);
             // score per minute
             double spm = playerList.get(player).getSpm() * 5;
+            System.out.println("SPM*5: " + spm);
 
             // top 25 / time played
             double top25Time = playerList.get(player).getTop25()/timePlayed * 100;
+            System.out.println("top25/time * 100: " + top25Time);
 
             // top 25 / gamesPlayed
             double top25Games = playerList.get(player).getTop25()/gamesPlayed * 100;
+            System.out.println("top25/games * 100: " + top25Games);
 
-            // top 25 / time played
-            double top5Time = playerList.get(player).getTop25()/timePlayed * 100;
+            // top 5 / time played
+            double top5Time = playerList.get(player).getTop5()/timePlayed * 100;
+            System.out.println("Top 5/time * 100: " + top5Time);
 
-            // top 25 / gamesPlayed * 10
-            double top5Games = playerList.get(player).getTop25()/gamesPlayed * 100;
+            // top 5 / gamesPlayed * 10
+            double top5Games = playerList.get(player).getTop5()/gamesPlayed * 100;
+            System.out.println("Top 25/games * 10: " + top5Games);
 
             // wins / timePlayed * 10
             double winsTime = playerList.get(player).getWins()/timePlayed * 200;
+            System.out.println("Wins/time * 10: "+winsTime);
 
             // wins / gamesPlayed * 20
             double winsGames = playerList.get(player).getWins()/gamesPlayed * 200;
+            System.out.println("wins/games * 200: " + winsGames);
 
-            // kd / gamesPlayed * 20
-            double kdGames = kd/gamesPlayed * 2;
+            // kd / gamesPlayed * 2000
+            double kdGames = playerList.get(player).getKd()/gamesPlayed * 2000;
+            System.out.println("kd/games * 2000: " + kdGames);
 
-
+            System.out.println("DATA END");
             // ADD UP THE SCORES
             scores[player] = kd + spm + top25Time + top25Games + top5Time
                     + top5Games + winsTime + winsGames + kdGames;
         }
 
         return scores;
+    }
+
+    // Print out the data
+    public static void printScores(double[] scores) throws PrinterException {
+        String[] columnNames = {"Name", "Score"};
+        Object[][] data = new Object[NUMOFGAMERTAGS][2];
+        for(int i = 0; i<NUMOFGAMERTAGS; i++){
+            data[i][0] = GAMERTAGS[i];
+            data[i][1] = scores[i];
+        }
+
+        JFrame jFrame=new JFrame();
+        JTable jTable = new JTable(data, columnNames);
+        jTable.setBounds(30,40,200,300);
+        JScrollPane sp=new JScrollPane(jTable);
+        jFrame.add(sp);
+        jFrame.setSize(400,400);
+        jFrame.setVisible(true);
     }
 }
